bb2aeba4999971c5c6576822edc42c3908252651
BrianG <brian@girardsolutions.com>
2019-04-23 10:50:17 -0400

    weighted
    weighted

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -15,11 +15,14 @@ namespace GradeBook.GradeBooks
 
         public GradeBookType Type { get; set; }
 
+        public bool IsWeighted { get; set; }
+
         public List<Student> Students { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
+            IsWeighted = isWeighted;
             Students = new List<Student>();
         }
 
@@ -109,18 +112,22 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            var weight = 0;
+            if (IsWeighted && (studentType == StudentType.DualEnrolled || studentType == StudentType.Honors))
+                weight = 1;
+
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    return 4 + weight;
                 case 'B':
-                    return 3;
+                    return 3 + weight;;
                 case 'C':
-                    return 2;
+                    return 2 + weight;;
                 case 'D':
-                    return 1;
+                    return 1 + weight;;
                 case 'F':
-                    return 0;
+                    return 0 + weight;;
             }
             return 0;
         }
